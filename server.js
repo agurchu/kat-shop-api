3b228668af88c62d1b1758d72df6dac47afb6ef4
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-09-07 11:35:46 +0200

    app update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDatabase();
 
 // create server
 const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on ${process.env.PORT}`);
 });
 
 // unhandled promise rejection
